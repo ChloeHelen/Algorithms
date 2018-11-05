3777777f3f32119318c7071e49aaa64ee9004943
ChloeHelen <cmingay@fastmail.fm>
2018-11-05 15:51:59 +0000

    Can define length of the array

--- timingCode/LastTiming.java
+++ timingCode/LastTiming.java
@@ -1,17 +1,27 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class LastTiming {
 
-    private static int[] arrayOne = new int[] {1,2,3};
-
     public static void main (String[] args) {
+        ArrayList<Integer> arrayOne = addToArray(10000);
         long startTime= System.nanoTime();
         int number = last(arrayOne);
         long endTime = System.nanoTime();
         System.out.println("It took " + (endTime - startTime) + " ns");
     }
 
-    public static int last(int[] arr) {
-        return arr[arr.length - 1];
+    public static int last(ArrayList<Integer> arr) {
+        return arr.get(arr.size() - 1);
+    }
+
+    public static ArrayList<Integer> addToArray(int times){
+        ArrayList<Integer> arrayOne = new ArrayList<Integer>();
+        int x = 1;
+        while (x <= times) {
+            arrayOne.add(1);
+            x++;
+        }
+        return arrayOne;
     }
 }
