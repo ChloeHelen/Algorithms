84871dfd527322d8a4afa8d667d21a2aa6ce71c8
ChloeHelen <cmingay@fastmail.fm>
2018-11-06 10:54:49 +0000

    Shuffle Timing

---
+++ timingCode/ShuffleTiming.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ShuffleTiming {
+
+    public static void main (String[] args) {
+        ArrayList<Integer> ArrayOne = addToArray(1000);
+        long startTime= System.nanoTime();
+        Collections.shuffle(ArrayOne);
+        long endTime = System.nanoTime();
+        System.out.println("It took " + (endTime - startTime) + " ns");
+    }
+
+    public static ArrayList<Integer> addToArray(int times){
+        ArrayList<Integer> arrayOne = new ArrayList<Integer>();
+        int x = 1;
+        while (x <= times) {
+            arrayOne.add(1);
+            x++;
+        }
+        return arrayOne;
+    }
+}
