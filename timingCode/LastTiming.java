0cb6deca332be66c86221058b2e6f25f54551173
ChloeHelen <cmingay@fastmail.fm>
2018-11-07 15:53:12 +0000

    Return average of 100 iterations

--- timingCode/LastTiming.java
+++ timingCode/LastTiming.java
@@ -6,10 +6,16 @@ public class LastTiming {
 
     public static void main (String[] args) {
         ArrayList<Integer> arrayOne = addToArray(1000);
-        long startTime= System.nanoTime();
-        int number = last(arrayOne);
-        long endTime = System.nanoTime();
-        System.out.println("It took " + (endTime - startTime) + " ns");
+        ArrayList<Long> resultsArray = new ArrayList<Long>();
+        int x = 1;
+        while (x < 101) {
+            long startTime= System.nanoTime();
+            int number = last(arrayOne);
+            long endTime = System.nanoTime();
+            resultsArray.add(endTime - startTime);
+            x ++;
+        }
+        System.out.println(averageArray(resultsArray));
     }
 
     public static int last(ArrayList<Integer> arr) {
@@ -20,9 +26,19 @@ public class LastTiming {
         ArrayList<Integer> arrayOne = new ArrayList<Integer>();
         int x = 1;
         while (x <= times) {
-            arrayOne.add(1);
+            arrayOne.add(x);
             x++;
         }
         return arrayOne;
+    }
+
+    public static long averageArray(ArrayList<Long> arr) {
+        int i;
+        long sum = 0;
+        for(i = 0; i < arr.size(); i++) {
+            sum += arr.get(i);
+        }
+        return (sum / (arr.size()));
+
     }
 }
